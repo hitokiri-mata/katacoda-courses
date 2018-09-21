ebb1021138d53ba6838d168a458f488e082059b2
Jonathan Johnson <jonathan.johnson@dijure.com>
2018-09-21 00:03:52 -0400

    fix fib logic

--- kubernetes-serverless/kubeless/assets/fibonacci.py
+++ kubernetes-serverless/kubeless/assets/fibonacci.py
@@ -1,16 +1,18 @@
-def fib(n):
+def fibonacci(n):
   if n == 0:
     return 0
   elif n == 1:
     return 1
   else:
-    return fib(n-1) + fib(n-2)
+    return fibonacci(n-1) + fibonacci(n-2)
+
+def sequence(length):
+  row = [fibonacci(n) for n in range(1,length)]
+  result = ','.join( str(x) for x in row )
+  print("Fibonacci sequence: " + result)
+  return {"sequence":result}
 
 def values(event, context):
-  length = event['data']['length']
-  print("Length: " + length)
-  print("Event.json: " + event.json)
-  row=[fib(n) for n in range(1,length)
-  serialized = ",".join( str(x) for x in row )
-  print("Fibonacci sequence: " + serialized)
-  return {"sequence":serialized}
+  sequence(event.data.length)
+  
+#print(sequence(18))
