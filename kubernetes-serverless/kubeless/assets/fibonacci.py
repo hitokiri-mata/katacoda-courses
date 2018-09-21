c8195d7bf22d90f856b34b14e57668f5d9263faa
Jonathan Johnson <jonathan.johnson@dijure.com>
2018-09-20 22:37:05 -0400

    Python parameters

--- kubernetes-serverless/kubeless/assets/fibonacci.py
+++ kubernetes-serverless/kubeless/assets/fibonacci.py
@@ -10,7 +10,7 @@ def values(event, context):
   length = event['data']['length']
   print("Length: " + length)
   print("Event.json: " + event.json)
-  row=[fib(n) for n in range(1,int(length)]
+  row=[fib(n) for n in range(1,length]
   serialized = ",".join( str(x) for x in row )
   print("Fibonacci sequence: " + serialized)
   return {"sequence":serialized}
