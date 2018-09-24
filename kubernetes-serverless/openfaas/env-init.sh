2209b68eb412ad2a9e3ee2aaa79072ebe55a5216
Jonathan Johnson <jonathan.johnson@dijure.com>
2018-09-24 01:38:46 -0400

    Adding fibonacci python to OpenFaaS

--- kubernetes-serverless/openfaas/env-init.sh
+++ kubernetes-serverless/openfaas/env-init.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 minikube config set WantUpdateNotification false
 clear
-minikube start
+minikube start --insecure-registry localhost:5000
+
+minikube addons enable registry
+
+# Using minikube's Docker daemon from our localhost
+eval $(minikube docker-env)
+
 helm repo update
 helm init --wait
+
+minikube status
+
+echo "Kubernetes using Minikube is ready."
