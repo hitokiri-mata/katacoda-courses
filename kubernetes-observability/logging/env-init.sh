64aed2eb3fe42211f017a4355210ee1cf295c8ca
Jonathan Johnson <jonathan.johnson@dijure.com>
2018-12-09 13:42:21 -0500

    EFK to kubernetes-cluster-running

--- kubernetes-observability/logging/env-init.sh
+++ kubernetes-observability/logging/env-init.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
 # Minikube pre-setup
-# minikube config set WantUpdateNotification false
+minikube config set WantUpdateNotification false
 
 # TODO: There is a known issue with Katacoda when this is applied. Waiting to move to new version of Minikube.
 # minikube config set bootstrapper kubeadm
-# source <(minikube completion bash)
-# source <(kubectl completion bash)
-# source <(helm completion bash)
-
-# clear
-
-launch.sh
+source <(minikube completion bash)
 source <(kubectl completion bash)
+source <(helm completion bash)
+
 clear
