f8da543ae4a1c76e25b28d5be0e4ddb8b6a05537
Jonathan Johnson <jonathan.johnson@dijure.com>
2019-01-20 18:57:59 -0500

    Added dashboard to OpenFaaS example

--- kubernetes-serverless/openfaas/env-init.sh
+++ kubernetes-serverless/openfaas/env-init.sh
@@ -3,6 +3,11 @@
 source <(kubectl completion bash)
 source <(helm completion bash)
 
+# Add Kubernetes dashboard at NodePort 30000
+kubectl create -n kube-system -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml 
+kubectl patch service kubernetes-dashboard --namespace=kube-system --type='json' --patch='[{"op": "replace", "path": "/spec/type","value":"NodePort"}]'
+kubectl patch service kubernetes-dashboard --namespace=kube-system --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
+
 # Helm Setup
 helm init --wait
 helm repo update
