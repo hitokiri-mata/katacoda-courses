c3e60dbb83e2d657f546a0fcd9fbb55da5560bbf
Ben Hall <ben@benhall.me.uk>
2019-06-25 13:05:19 +0100

    Apply Kuberentes Dashboard NodePort service

--- kubernetes-fundamentals/first-app/env-init.sh
+++ kubernetes-fundamentals/first-app/env-init.sh
@@ -10,7 +10,4 @@ minikube addons enable metrics-server
 
 # Dashboard for Minikube on port 30000
 minikube addons enable dashboard
-sleep 9
-kubectl delete service kubernetes-dashboard -n kube-system
-kubectl expose deployment kubernetes-dashboard -n kube-system --type=NodePort
-kubectl patch service kubernetes-dashboard --namespace=kube-system --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
+kubectl apply -f /opt/kubernetes-dashboard.yaml
