8878797c2a7de1340676981d8e3e50d000b35928
Jonathan Johnson <jonathan.johnson@dijure.com>
2019-08-04 10:36:18 -0400

    Upgraded to faster minikube instance.

--- kubernetes-applications/r-shiny/env-init.sh
+++ kubernetes-applications/r-shiny/env-init.sh
@@ -3,10 +3,11 @@ set +x
 
 sleep 1; launch.sh
 
-source <(minikube completion bash)
-source <(kubectl completion bash)
-source <(helm completion bash)
-minikube addons enable dashboard
-kubectl create -f /opt/kubernetes-dashboard.yaml
-
-echo "Your Kubernetes cluster, based on Minikube, is ready."
+{
+  source <(minikube completion bash);
+  source <(kubectl completion bash);
+  source <(helm completion bash);
+  minikube addons enable dashboard;
+  kubectl create -f /opt/kubernetes-dashboard.yaml;
+  echo "Your Kubernetes cluster, based on Minikube, is ready.";
+} 2> /dev/null
