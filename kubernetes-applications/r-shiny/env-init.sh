7055f820b019fd47877e6215b3c7965fb5d81284
Jonathan Johnson <jonathan.johnson@dijure.com>
2019-08-04 07:19:42 -0500

    Upgraded to faster minikube instance.

--- kubernetes-applications/r-shiny/env-init.sh
+++ kubernetes-applications/r-shiny/env-init.sh
@@ -12,9 +12,9 @@ clear
 # minikube start 
 
 # Dashboard for Minikube on port 30000
-minikube addons enable dashboard
-sleep 10
-kubectl get services --all-namespaces
-kubectl delete service kubernetes-dashboard -n kube-system
-kubectl expose deployment kubernetes-dashboard -n kube-system --type=NodePort
-kubectl patch service kubernetes-dashboard --namespace=kube-system --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
+# minikube addons enable dashboard
+# sleep 10
+# kubectl get services --all-namespaces
+# kubectl delete service kubernetes-dashboard -n kube-system
+# kubectl expose deployment kubernetes-dashboard -n kube-system --type=NodePort
+# kubectl patch service kubernetes-dashboard --namespace=kube-system --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
