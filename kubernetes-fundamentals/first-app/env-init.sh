80f9d0d3ba971f53c33d2f669294ba024949b756
Jonathan Johnson <jonathan.johnson@dijure.com>
2019-10-02 18:42:41 -0400

    Add metrics-server

--- kubernetes-fundamentals/first-app/env-init.sh
+++ kubernetes-fundamentals/first-app/env-init.sh
@@ -12,4 +12,7 @@ helm repo update
 # Setup dashboard on port 30000
 helm install stable/kubernetes-dashboard --name dash --set=service.type=NodePort --set=enableInsecureLogin=true --set=service.nodePort=30000 --set=service.externalPort=80 --namespace kube-system
 
+# Enable metrics
+helm install --name metrics-server --namespace kube-system stable/metrics-server
+
 { clear && echo 'Kubernetes with Helm is ready.'; } 2> /dev/null
