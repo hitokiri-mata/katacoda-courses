8e200173a37bb7f4fa37049f1b3aa5ea65a6cd0a
Jonathan Johnson <jonathan.johnson@dijure.com>
2020-02-23 14:21:44 -0500

    Add ML Course with Kubeflow draft.

--- kubernetes-ml/kubeflow/env-init.sh
+++ kubernetes-ml/kubeflow/env-init.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 # Log for analysis of startup
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log.out 2>&1
-# Everything below will go to the file 'log.out':
+set -x
 
 launch.sh
 
@@ -87,4 +84,4 @@ source <(kfctl completion bash)
 source <(kubectl completion bash)
 source <(helm completion bash)
 
-{ clear && echo 'Kubernetes with Kubeflow and Helm is ready.'; } 2> /dev/null
+# { clear && echo 'Kubernetes with Kubeflow and Helm is ready.'; } 2> /dev/null
