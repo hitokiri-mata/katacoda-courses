4ed08238aef08e930aff0f1466187739066b42c3
Jonathan Johnson <jonathan.johnson@dijure.com>
2020-05-31 23:18:23 -0400

    Removed ccat

--- kubernetes-containers/go/env-init.sh
+++ kubernetes-containers/go/env-init.sh
@@ -7,12 +7,12 @@ echo '-s' > ~/.curlrc
 
 # Allow pygmentize for source highlighting
 docker pull whalebrew/pygmentize
-echo 'function ccat() { docker run -it -v "$(pwd)":/workdir -w /workdir whalebrew/pygmentize $1; }' >> ~/.bashrc
+echo 'function ccat() { docker run -it -v "$(pwd)":/workdir -w /workdir     whalebrew/pygmentize $1; }' >> ~/.bashrc
 source ~/.bashrc
 
 # Helm setup
 HELM_VERSION=v3.1.2
-curl -fsSL https://get.helm.sh/helm-$HELM_VERSION-linux-amd64.tar.gz | tar -zxvf - -C /usr/local/bin/ linux-amd64/helm --strip=1
+    curl -fsSL https://get.helm.sh/helm-$HELM_VERSION-linux-amd64.tar.gz | tar -zxvf - -C /usr/local/bin/ linux-amd64/helm --strip=1
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 
 # Setup dashboard on port 30000
--- kubernetes-containers/oci/env-init.sh
+++ kubernetes-containers/oci/env-init.sh
@@ -5,11 +5,6 @@ launch.sh
 # Common curl switches
 echo '-s' > ~/.curlrc
 
-# Allow pygmentize for source highlighting
-docker pull whalebrew/pygmentize
-echo 'function ccat() { docker run -it -v "$(pwd)":/workdir -w /workdir whalebrew/pygmentize $1; }' >> ~/.bashrc
-source ~/.bashrc
-
 apt update -qq
 apt install tree wget --yes -qq
 
