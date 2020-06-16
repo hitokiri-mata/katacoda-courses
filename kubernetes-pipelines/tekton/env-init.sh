feb4e9ab693535ea1e3a2ba968ab12c6ee1c9e15
Jonathan Johnson <jonathan.johnson@dijure.com>
2020-06-16 15:56:58 -0400

    Adjusted Tekton senario, K8s 1.18

--- kubernetes-pipelines/tekton/env-init.sh
+++ kubernetes-pipelines/tekton/env-init.sh
@@ -2,29 +2,16 @@
 
 launch.sh
 
-# Helm setup
-# HELM_VERSION=v3.1.2
-# curl -fsSL https://get.helm.sh/helm-$HELM_VERSION-linux-amd64.tar.gz | tar -zxvf - -C /usr/local/bin/ linux-amd64/helm --strip=1
-# helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-
 # Setup dashboard on port 30000
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm install kubernetes-dashboard/kubernetes-dashboard \
---name kubernetes-dashboard \
+helm install dash kubernetes-dashboard/kubernetes-dashboard \
+--namespace kube-system \
 --set=service.type=NodePort \
 --set=enableInsecureLogin=true \
 --set=service.nodePort=30000 \
 --set=service.externalPort=80
 
 source <(kubectl completion bash)
-# source <(helm completion bash)
-
-# apt update
-# apt install tree --yes -qq
-
-# Workaround: Katacoda base image needs fixing
-# JAVA_HOME is set to JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin, should be:
-# export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+source <(helm completion bash)
 
 { clear && echo 'Kubernetes with Helm is ready.'; } 2> /dev/null
-
