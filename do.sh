9793d0e7808d5e10095557c40733af08af8563be
Jonathan Johnson <jonathan.johnson@dijure.com>
2020-07-23 21:29:31 -0400

    Migrating to K8s 1.18, init foreground and background scripts.

--- do.sh
+++ do.sh
@@ -1,4 +1,4 @@
 find . -name 'init-foreground.sh' -print0 | while read -d $'\0' file
 do
-    cp spin.sh $file
+    cp spin.sh $(dirname $file)/init-foreground.sh
 done
--- kubernetes-fundamentals/jobs/init-foreground.sh
+++ kubernetes-fundamentals/jobs/init-foreground.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
 
-show_progress()
-{
-  local -r pid="${1}"
-  local -r delay='0.75'
-  local spinstr='\|/-'
-  local temp
-
-  echo -n "Starting scenario"
-  while true; do
-    sudo grep -i "done" /opt/.backgroundfinished &> /dev/null
-    if [[ "$?" -ne 0 ]]; then
-      temp="${spinstr#?}"
-      printf " [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep "${delay}"
-      printf "\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-  printf "    \b\b\b\b"
-  echo ""
-  echo "You have a running Kubernetes cluster."
-}
-
-show_progress
+source ./init-progress.sh
+clear && show_progress
