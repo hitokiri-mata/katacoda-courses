ebd4a61791a4675e73c467106ef045815d244a0f
Jonathan Johnson <jonathan.johnson@dijure.com>
2020-08-21 11:53:39 -0400

    drafting pure-chaos

--- kubernetes-chaos/pure-chaos/assets/snowflake_melter.py
+++ kubernetes-chaos/pure-chaos/assets/snowflake_melter.py
@@ -1,24 +1,20 @@
 from kubernetes import client, config
-from random import randint, choice
+import random
 
 config.load_incluster_config()
-
 v1=client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
 candidates = []
 for i in ret.items:
-    if i.metadata.annotations["chaos"].lower() == 'yes':
+    if i.metadata.annotations is not None and i.metadata.annotations["chaos"] == 'yes':
         candidates.append(i)
 
-logger.debug("Found {d} pods to potentially remove.".format(d=len(candidated)))
+removal_count = random.randint(0, len(candidates)-1)
+logger.info("Found %d pods and will melt %d of them.", len(candidates), removal_count)
 
-for i in range(0, random.randint(0, len(candidates)-1)):
+for i in range(0, removal_count):
     pod = random.choice(candidates)
     candidates.remove(pod)
-    print("Removing pod: %s from namespace %s", item.metadata.name, item.metadata.namespace)
+    logger.info("Removing pod %s from namespace %s.", item.metadata.name, item.metadata.namespace)
     body = client.V1DeleteOptions()
     v1.delete_namespaced_pod(pod.metadata.name, pod.metadata.ns, body=body) 
